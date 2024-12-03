593284321378a7d51617a68786d013ce2d81313f
cgdouthitt <46352253+cgdouthitt@users.noreply.github.com>
2024-12-03 10:22:20 -0600

    Extra credit changes
    - updating profile pic
    - adding error handling views
    - adding css changes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # techdegree-project-6
- Live site for project display
+
+Live site for project display
+
+CSS customs:
+.sidebar p color changed to #bfbcbc
+.sidebar a color changed to #bfbcbc
+.cell img added transition zoom on hover
+.btn-link:hover added opacity: .8
--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.set("view engine", "pug");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "images")));
 
 //Home route
 app.get("/", (req, res) => {
@@ -21,32 +20,38 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-//project route based on id
-app.get("/projects/:id", (req, res) => {
+//Project route based on id
+app.get("/projects/:id", (req, res, next) => {
   const id = +req.params.id;
-  const project = projects.find((project) => project.id === id);
-  res.render("project", { project });
+  if (typeof id === "number" && id <= projects.length - 1) {
+    const project = projects.find((project) => project.id === id);
+    res.render("project", { project });
+  } else {
+    next();
+  }
 });
 
-// error handlers
+// Error handlers
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use((req, res, next) => {
-  console.log("404 error handler called");
-  res.status(404).render("page-not-found");
+  const err = new Error("Looks like this page doesn't exsist.");
+  err.status = 404;
+  console.log("404 error handler called", err);
+  res.status(404).render("page-not-found", { err });
 });
 
-// server error handler
+// Global error handler
 app.use((err, req, res, next) => {
   if (err) {
     console.log("Global error handler called", err);
   }
-  console.log(err.status);
   if (err.status === 404) {
     res.status(404).render("page-not-found", { err });
   } else {
     err.message = err.message || "Something went wrong with the server.";
-    res.status(err.status || 500).render("error", { err });
+    err.status = 500;
+    res.status(500).render("error", { err });
   }
 });
 
